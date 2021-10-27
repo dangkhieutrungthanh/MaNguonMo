a2ed96515bdf5d6114d582c74f23b092b509f159
dangkhieutrungthanh <khieuthanhtb19@gmail.com>
2021-10-27 13:25:08 +0700

    done

--- Controllers/HelloWorldController.cs
+++ Controllers/HelloWorldController.cs
@@ -1,7 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using System.Text.Encodings.Web;
 
-namespace MvcMovie.Controllers
+namespace dotnet.Controllers
 {
     public class HelloWorldController : Controller
     {
