c148f3d8958194d8ab318e951e77be168add2906
dangkhieutrungthanh <khieuthanhtb19@gmail.com>
2021-10-27 13:15:25 +0700

    add a controller

--- Startup.cs
+++ Startup.cs
@@ -50,7 +50,7 @@ namespace dotnet
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                     pattern: "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
